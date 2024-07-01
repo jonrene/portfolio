625ef9c5d61b898e923c32a3ca416a4801b70da9
jonrene <jrjonathanrene@gmail.com>
2024-07-01 14:30:18 -0400

    add youtube clone to projects

--- src/projectsData.jsx
+++ src/projectsData.jsx
@@ -1,5 +1,13 @@
 const projectsData = [
   {
+    title: "YouTube.com Clone",
+    description: "Created a fully functional replica of the YouTube homepage with fully interactive elements.",
+    tools: "ReactJS, HTML, CSS, NodeJS, Vite",
+    photo: "./src/assets/projectPhotos/youtube.png",
+    code: "https://github.com/jonrene/youtubeclone",
+    demo: "https://jonrene.github.io/youtubeclone/",
+  },
+  {
       title: "Roll20 Pathfinder Second Edition Character Sheet",
       description: "Contributed to the development of the official Roll20 Pathfinder Second Edition Character Sheet by Paizo",
       tools: "Vuejs, Typescript, Sass, Pinia, Vite, Vitest",
@@ -10,7 +18,7 @@ const projectsData = [
   {
     title: "Roll20 Marvel Multiverse Role-Playing Game Character Sheet",
     description: "Contributed to the development of the official Roll20 Marvel Multiverse RPG Character Sheet by Marvel",
-    tools: "Vuejs, Typescript, Sass, Pinia, Vite, Vitest",
+    tools: "Vuejs, Typescript, NodeJS, Sass, Pinia, Vite, Vitest",
     photo: "./src/assets/projectPhotos/marvel.png",
     code: "https://pages.roll20.net/marvel-multiverse",
     demo: "https://www.youtube.com/watch?v=gQGOV4XxgWI&ab_channel=NickOlivo",
@@ -18,13 +26,11 @@ const projectsData = [
   {
     title: "Roll20 Dungeons and Dragons Official Character Sheet",
     description: "Contributed to the development of the official Dungeons and Dragons Character Sheet",
-    tools: "Vuejs, Typescript, Sass, Pinia, Vite, Vitest",
+    tools: "Vuejs, Typescript, NodeJS, Sass, Pinia, Vite, Vitest",
     photo: "./src/assets/projectPhotos/dnd.png",
     code: "https://blog.roll20.net/posts/introducing-the-new-roll20-dungeons-dragons-character-sheet/",
     demo: "https://www.youtube.com/watch?v=FIOJnGmXui8&t=12s&ab_channel=Roll20",
   },
-
-  
 ]
 
 export default projectsData;
